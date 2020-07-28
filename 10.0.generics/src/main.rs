30096782c7c9fefdf0a96ab4b1598931412eaf82
Kreedzt <zhaozisong1@live.com>
2020-07-28 16:06:37 +0800

    :sparkles: Kreedzt - complete chapter 10 introduction

---
+++ 10.0.generics/src/main.rs
@@ -0,0 +1,42 @@
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    // let mut largest = number_list[0];
+
+    // for number in number_list {
+    //     if number > largest {
+    //         largest = number;
+    //     }
+    // }
+
+    // println!("The largest number is {}", largest);
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    // let mut largest = number_list[0];
+
+    // for number in number_list {
+    //     if number > largest {
+    //         largest = number;
+    //     }
+    // }
+
+    // println!("The largest number is {}", largest);
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
