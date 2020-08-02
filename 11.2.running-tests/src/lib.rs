8ea9e2cc11e4b204fd856e24948859ba785bac6b
Kreedzt <zhaozisong1@live.com>
2020-08-02 14:03:15 +0800

    :sparkles: Kreedzt - completed to 11.2

---
+++ 11.2.running-tests/src/lib.rs
@@ -0,0 +1,52 @@
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(14);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(100, add_two(100));
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // 需要长时间运行的代码
+        // 给定 #[ignore] 忽略测试
+    }
+}
