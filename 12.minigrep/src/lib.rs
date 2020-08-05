a9e1f7cd0f02408dfc7ec8ad5090a93d4879a5c0
Kreedzt <zhaozisong1@live.com>
2020-08-05 20:29:30 +0800

    :sparkles: Kreedzt - completed to 12.3

---
+++ 12.minigrep/src/lib.rs
@@ -0,0 +1,30 @@
+use std::{fs, error::Error};
+
+pub struct Config {
+    pub query: String,
+    pub filename: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // let contents = fs::read_to_string(config.filename)
+    //     .expect("Something went wrong reading the file");
+
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
--- 12.minigrep/src/main.rs
+++ 12.minigrep/src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs, env};
+// use std::{fs, env, process, error::Error};
+use std::{env, process};
+use minigrep::Config;
+
 
 fn main() {
     // collect 是一个经常需要注明类型的函数, 因为 Rust 不能推断出想要的类型集合
@@ -15,29 +18,66 @@ fn main() {
     // let contents = fs::read_to_string(filename)
     //     .expect("Something went wrong reading the file");
 
-    let config = parse_config(&args);
+    // let config = parse_config(&args);
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.filename);
 
-    println!("With text:\n{}", contents);
+    // let contents = fs::read_to_string(config.filename)
+    //     .expect("Something went wrong reading the file");
+
+    // println!("With text:\n{}", contents);
+
+    // 警告: 没有错误处理
+    // run(config);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
+// fn run(config: Config) -> Result<(), Box<dyn Error>> {
+//     // let contents = fs::read_to_string(config.filename)
+//     //     .expect("Something went wrong reading the file");
 
-fn parse_config(args: &[String]) -> Config {
-    // clone 拥有数据的完整拷贝, 无需管理引用的生命周期
-    let query = args[1].clone();
-    let filename = args[2].clone();
+//     let contents = fs::read_to_string(config.filename)?;
 
-    Config { query, filename }
-}
+//     println!("With text:\n{}", contents);
+
+//     Ok(())
+// }
+
+// struct Config {
+//     query: String,
+//     filename: String,
+// }
+
+// impl Config {
+//     fn new(args: &[String]) -> Result<Config, &'static str> {
+//         if args.len() < 3 {
+//             return Err("not enough arguments");
+//         }
+
+//         let query = args[1].clone();
+//         let filename = args[2].clone();
+
+//         Ok(Config { query, filename })
+//     }
+// }
+
+// fn parse_config(args: &[String]) -> Config {
+//     // clone 拥有数据的完整拷贝, 无需管理引用的生命周期
+//     let query = args[1].clone();
+//     let filename = args[2].clone();
+
+//     Config { query, filename }
+// }
 
 // fn parse_config(args: &[String]) -> (&str, &str) {
 //     let query = &args[1];
