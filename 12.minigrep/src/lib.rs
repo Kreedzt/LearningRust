b38a5dbc00ea711ecfad91515d40fb3484a084cc
Kreedzt <zhaozisong1@live.com>
2020-08-06 09:50:24 +0800

    :sparkles: Kreedzt - completed to 12.4

--- 12.minigrep/src/lib.rs
+++ 12.minigrep/src/lib.rs
@@ -24,7 +24,45 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    // println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        )
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    // vec![]
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
